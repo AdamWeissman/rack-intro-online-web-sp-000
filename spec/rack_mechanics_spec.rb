1e91aea3aac42924c35012d0b6cbfba1bec54ec0
Adam Weissman <adam@comedytragedyepic.com>
2019-12-07 11:10:12 +0000

    Automatically backed up by Learn

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper.rb'
 
-#describe "Application" do
+describe "Application" do
 #  it "should introduce itself" do
 #    get "/"
 #    expect(last_response.body).to include ("Hello, my name is")
